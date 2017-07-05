86562e1a369f8e8be298d18ba5d2417439c502bc
k.c. schroeter <kesc0188@colorado.edu>
2017-07-04 22:52:19 -0400

    Keep only stache on face

--- examples/deep_moustache.py
+++ examples/deep_moustache.py
@@ -9,14 +9,15 @@ image = face_recognition.load_image_file("biden.jpg")
 # Find all facial features in all the faces in the image
 face_landmarks_list = face_recognition.face_landmarks(image)
 
-#Is this line necessary
-#for face_landmarks in face_landmarks_list:
-pil_image = Image.fromarray(image)
-d = ImageDraw.Draw(pil_image, 'RGBA')
+for face_landmarks in face_landmarks_list:
+    #Is this line necessary
+    #for face_landmarks in face_landmarks_list:
+    pil_image = Image.fromarray(image)
+    d = ImageDraw.Draw(pil_image, 'RGBA')
 
-# Change color
-# Make width 75% of distance between lip and nose. 
-# Move line up 50% of the width in pixel diraction
-d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
+    # Change color
+    # Make width 75% of distance between lip and nose.
+    # Move line up 50% of the width in pixel diraction
+    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
 
-pil_image.show()
+    pil_image.show()
