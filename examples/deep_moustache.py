9897c2d3b8a8d9809e99336d1822a5e5d99b6f29
k.c. schroeter <kesc0188@colorado.edu>
2017-07-08 12:49:09 -0400

    Find Line Slopes and Nose to Lip Distance

--- examples/deep_moustache.py
+++ examples/deep_moustache.py
@@ -17,9 +17,11 @@ d = ImageDraw.Draw(pil_image, 'RGBA')
     # Make width 75% of distance between lip and nose.
     # Move line up 50% of the width in pixel diraction
 topLip = face_landmarks_list[0]['top_lip']
+noseTip = face_landmarks_list[0]['nose_tip']
 
 #Convert from Tuple to List so we can alter
 topLipList = [list(elm) for elm in topLip]
+noseTipList = [list(elm) for elm in noseTip]
 #print(topLipList)
 
 #Make Line the width of the lip
@@ -31,24 +33,33 @@ print(topLipWidth)
 print(topLipWidth[0][0])
 print(topLipWidth[1][0])
 
+#Make Line between nose tip and middle of lip
+noseLipDist = list()
+noseLipDist.append(noseTipList[2])
+noseLipDist.append(topLipList[3])
+
 #Make Line the Height of the lip
 topLipHeight = list()
 topLipHeight.append(topLipList[3])
 topLipHeight.append(topLipList[10])
 #print(topLipHeight)
 
+lipWidthVal = math.sqrt((((topLipWidth[1][0])-(topLipWidth[0][0]))**(2))+(((topLipWidth[1][1])-(topLipWidth[0][1]))**(2)))
+noseLipDistVal = math.sqrt((((noseLipDist[1][0])-(noseLipDist[0][0]))**(2)+(((noseLipDist[1][1])-(noseLipDist[0][1]))**(2))))
+lipSlope = ((topLipWidth[1][1])-(topLipWidth[0][1]))/((topLipWidth[1][0])-(topLipWidth[0][0]))
+recipLipSlope = (-1)*(1/lipSlope)
+
+stacheWidth = topLipWidth
+stacheWidth = stacheWidth * int(round(noseLipDistVal * recipLipSlope))
+
 topLipWidth1 = [tuple(elm) for elm in topLipWidth]
 topLipHeight1 = [tuple(elm) for elm in topLipHeight]
 topLip1 = [tuple(elm) for elm in topLipList]
-#print(topLipWidth1)
-#print(topLipHeight1)
-#print(topLip1)
-
-widthVal = math.sqrt((((topLipWidth[1][0])-(topLipWidth[0][0]))**(2))+(((topLipWidth[1][1])-(topLipWidth[0][1]))**(2)))
+stacheWidth1 = [tuple(elm) for elm in stacheWidth]
 
 #print(topLip)
 #d.line(topLipWidth1, fill=(150,150,150,150), width=3)
 #d.line(topLipHeight1, fill=(150,150,150,150), width=3)
-d.line(topLipWidth1, fill=(255,255,255,255), width=int(round(widthVal/10)))
+d.line(stacheWidth1, fill=(255,255,255,255), width=8)
 
 pil_image.show()
